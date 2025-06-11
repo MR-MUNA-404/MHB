3fc8e13cd80a43ef12e62b194cda29d1ed1fec11
MR-MUNA-404 <136841898+MR-MUNA-404@users.noreply.github.com>
2025-06-11 23:29:19 +0500

    Update MHB.py

--- MHB.py
+++ MHB.py
@@ -4,10 +4,5 @@ print("Join my Whatsapp Group")
 os.system('xdg-open https://chat.whatsapp.com/HdppaiQyFtZ1AwiOzc5iEe')
 import os
 os.system('git pull')
-import os
-print("")
-print("\033[1;32mSorry Tool Server done ")
-print("\033[1;37m\033[1;32mTool is offline")
-
-
+import MHB
                
