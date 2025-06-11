71e6c8b25090385aff07167e6b47e901733ca170
MR-MUNA-404 <136841898+MR-MUNA-404@users.noreply.github.com>
2025-06-11 16:11:44 +0500

    Update MHB.py

--- MHB.py
+++ MHB.py
@@ -5,8 +5,9 @@ os.system('xdg-open https://chat.whatsapp.com/HdppaiQyFtZ1AwiOzc5iEe')
 import os
 os.system('git pull')
 import os
-print("Sorry Tool Server done ")
-print("Tool is offline")
+print("")
+print("\033[1;32mSorry Tool Server done ")
+print("\033[1;37m\033[1;32mTool is offline")
 
 
                
